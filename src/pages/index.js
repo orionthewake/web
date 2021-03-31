8332eed0689eeffad3cb7984da6b75c713346b5d
Joseph Howard <joe.howard@mac.com>
2021-03-31 16:56:05 -0400

    Protected routes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 // import React and routing dependencies
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+import { useQuery, gql } from '@apollo/client'; 
 
 // import shared Layout component
 import Layout from '../components/Layout';
@@ -13,19 +14,55 @@ import NotePage from './note';
 import SignUp from './signup';
 import SignIn from './signin';
 
+const IS_LOGGED_IN = gql`
+  {
+    isLoggedIn @client
+  }
+`;
+
 // define routes
 const Pages = () => {
   return (
     <Router>
       <Layout>
         <Route exact path="/" component={Home} />
-        <Route path="/mynotes" component={MyNotes} />
-        <Route path="/favorites" component={Favorites} />
+        <PrivateRoute path="/mynotes" component={MyNotes} />
+        <PrivateRoute path="/favorites" component={Favorites} />
         <Route path="/note/:id" component={NotePage} />
         <Route path="/signup" component={SignUp} />
         <Route path="/signin" component={SignIn} />
       </Layout>
     </Router>
+  );
+};
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  const { loading, error, data } = useQuery(IS_LOGGED_IN);
+
+  // if the data is loading, show a loading message
+  if (loading) return <p>Loading...</p>;      
+
+  // if there is an error fetching the data, show an error message
+  if (error) return <p>Error!</p>;
+
+  // if the user is logged in, route them to the requested component
+  // else redirect them to the sign-in page
+  return (
+    <Route
+      {...rest}
+      render={props => 
+        data.isLoggedIn === true ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{
+              pathname: '/signin',
+              state: { from: props.location }
+            }}
+          />
+        )
+      }
+    />
   );
 };
 
